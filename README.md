d5a59866ccd6d7e60114c17d837e3c025bdc539c
Esron Silva <essilva@redhat.com>
2025-12-10 10:09:43 -0300

    use argparse to simplify setup

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Advent of Python 
+# Advent of Python
 
 My solutions to [Advent of Code](https://adventofcode.com) implemented as a Python program.
 
-This repository holds my solutions to Advent of Code problems and my experiments with [Click](https://click.palletsprojects.com/en/8.0.x/), a Python framework for creating cli tools.
+This repository holds my solutions to Advent of Code problems implemented as a Python CLI tool using standard library `argparse`.
 
 ## Setup
 
@@ -22,10 +22,6 @@ Install dependencies and build:
 
 ```bash
 pip install .
-
-# or use this for development mode so rebuild/reinstall isn't necessary after 
-# each change that is made during development
-pip install -e .
 ```
 
 Install dev dependencies:
@@ -37,15 +33,15 @@ pip install -r requirements.txt
 ## Usage
 
 ```bash
-adventofpy [OPTIONS]
+python adventoofpy.py [OPTIONS]
 ```
 
 ## Options
 ```
-  -y, --year INTEGER RANGE  Event year  [2015<=x<=2021; required]
-  -d, --day INTEGER RANGE   Problem day  [1<=x<=25; required]
-  -p, --part [1|2]          Problem part  [required]
-  --help                    Show this message and exit.
+  -y, --year YEAR    Event year (2015-2025) [required]
+  -d, --day DAY      Problem day (1-25) [required]
+  -p, --part PART    Problem part (1 or 2) [required]
+  -h, --help         Show this help message and exit
 ```
 
 ## Stats
@@ -61,5 +57,3 @@ adventofpy [OPTIONS]
 | **Day**   | 1            | 2            | 3            | 4            | 5            | 6            | 7            | 8            | 9            | 10           | 11           | 12           | 13           | 14           | 15           | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24 | 25 |
 |-----------|--------------|--------------|--------------|--------------|--------------|--------------|--------------|--------------|--------------|--------------|--------------|--------------|--------------|--------------|--------------|----|----|----|----|----|----|----|----|----|----|
 | **Stars** | :star::star: | :star::star: | :star::star: | :star::star: | :star::star: | :star::star: | :star::star: | :star::star: | :star::star: | :star::star: | :star::star: | :star::star: | :star::star: | :star::star: | :star::star: |    |    |    |    |    |    |    |    |    |    |
-
-
--- adventofpy.py
+++ adventofpy.py
@@ -1,22 +1,65 @@
-import click
+import argparse
+import sys
 from importlib import import_module
 
 
-@click.command()
-@click.option('-y', '--year', type=click.IntRange(2015, 2023), required=True,
-              help='Event year')
-@click.option('-d', '--day', type=click.IntRange(1, 25), required=True,
-              help='Problem day')
-@click.option('-p', '--part', type=click.Choice(['1', '2']), required=True,
-              help='Problem part')
-def cli(year: int, day: int, part: str):
-    """Advent of Python
+def validate_year(value):
+    """Validate year is between 2015 and 2025."""
+    year = int(value)
+    if year < 2015 or year > 2025:
+        raise argparse.ArgumentTypeError(f'Year must be between 2015 and 2025, got {year}')
+    return year
 
-       Python solutions to Advent of Code."""
-    click.echo(f'Running script for year: {year}, day: {day:02d}, part: {part}')
+
+def validate_day(value):
+    """Validate day is between 1 and 25."""
+    day = int(value)
+    if day < 1 or day > 25:
+        raise argparse.ArgumentTypeError(f'Day must be between 1 and 25, got {day}')
+    return day
+
+
+def validate_part(value):
+    """Validate part is either 1 or 2."""
+    if value not in ['1', '2']:
+        raise argparse.ArgumentTypeError(f'Part must be 1 or 2, got {value}')
+    return value
+
+
+def cli():
+    """Advent of Python - Python solutions to Advent of Code."""
+    parser = argparse.ArgumentParser(
+        description='Advent of Python - Python solutions to Advent of Code.'
+    )
+    parser.add_argument(
+        '-y', '--year',
+        type=validate_year,
+        required=True,
+        help='Event year (2015-2023)'
+    )
+    parser.add_argument(
+        '-d', '--day',
+        type=validate_day,
+        required=True,
+        help='Problem day (1-25)'
+    )
+    parser.add_argument(
+        '-p', '--part',
+        type=validate_part,
+        required=True,
+        help='Problem part (1 or 2)'
+    )
+
+    args = parser.parse_args()
+
+    print(f'Running script for year: {args.year}, day: {args.day:02d}, part: {args.part}')
     try:
-        script = import_module(f'.part{part}', f'year_{year}.day{day:02d}')
+        script = import_module(f'.part{args.part}', f'year_{args.year}.day{args.day:02d}')
         script.run()
     except ModuleNotFoundError:
-        click.echo(f'Module year_{year}.day{day:02d}.part{part} not found.', err=True)
-        exit(1)
+        print(f'Module year_{args.year}.day{args.day:02d}.part{args.part} not found.', file=sys.stderr)
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    cli()
